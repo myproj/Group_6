004a94d6a481fea920e23f06c6f9df95e4329052
snkreddy <snkreddy1gmail.com>
2017-08-22 16:56:15 +0530

    good

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,13 @@
 #include "functions.h"
-
+#include <iostream>
+//recurrision
 int factorial(int n){
-    if(n!=1){
+    if(n<0)
+    {
+        std::cout<<"no negative";
+        return -1;
+    }
+    if(n!=1&&n>1){
 	return(n * factorial(n-1));
     }
     else return 1;
