6fc2ed230c9cee2ad0e46a04afb45ddfca8c2a1a
snkreddy <snkreddy1gmail.com>
2017-08-22 16:59:28 +0530

    too good

--- factorial.cpp
+++ factorial.cpp
@@ -1,10 +1,11 @@
 #include "functions.h"
 #include <iostream>
 //recurrision
+//arc
 int factorial(int n){
     if(n<0)
     {
-        std::cout<<"no negative";
+        std::cout<<"no negative ";
         return -1;
     }
     if(n!=1&&n>1){
